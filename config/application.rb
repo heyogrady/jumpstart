ef0b7edb320f0e318ef26968c9d6aaacb9ce8757
Patrick O'Grady <ogradypatrickj@gmail.com>
2015-05-07 14:11:25 -0400

    Remove generators configuration

--- config/application.rb
+++ config/application.rb
@@ -14,16 +14,6 @@ module Jumpstart
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.generators do |generate|
-      generate.helper false
-      generate.javascript_engine false
-      generate.request_specs false
-      generate.routing_specs false
-      generate.stylesheets false
-      generate.test_framework :rspec
-      generate.view_specs false
-    end
-
     config.action_controller.action_on_unpermitted_parameters = :raise
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
