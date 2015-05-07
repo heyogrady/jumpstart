a1a726ef52f231f0faa86cbb2292f04c33e8fcfc
Patrick O'Grady <ogradypatrickj@gmail.com>
2015-05-07 13:28:40 -0400

    Tweak unicorn settings

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,27 +1,32 @@
 # this template has been picked up from
 # https://devcenter.heroku.com/articles/rails-unicorn
 
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
-
-# 30 seconds is the timeout for heroku. If it is not
-# deployed on heroku then it can be bumped up.
-timeout 30
-
+worker_processes (ENV["WEB_CONCURRENCY"] || 3).to_i
+timeout (ENV["UNICORN_TIMEOUT"] || 15).to_i
 preload_app true
 
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+before_fork do |_server, _worker|
+  Signal.trap "TERM" do
+    puts "Unicorn master intercepting TERM, sending myself QUIT instead"
+    Process.kill "QUIT", Process.pid
   end
 
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
 end
 
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+after_fork do |_server, _worker|
+  Signal.trap "TERM" do
+    puts "Unicorn worker intercepting TERM and doing nothing..."
+    puts "...waiting for master to send QUIT"
   end
 
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+    Rails.application.config.database_configuration[Rails.env]
+    config["reaping_frequency"] = (ENV["DB_REAPING_FREQUENCY"] || 10).to_i
+    config 
+    ActiveRecord::Base.establish_connection
+  end
 end
