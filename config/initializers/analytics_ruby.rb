3e5301ab144f1c320a124fc8f7b0a0627fdfaf86
Patrick O'Grady <ogradypatrickj@gmail.com>
2015-05-07 13:28:20 -0400

    Add config files
    Config for:
    * Segment.io
    * Stripe
    * Bullet
    * Twilio
    * New Relic
    * i18n-tasks

---
+++ config/initializers/analytics_ruby.rb
@@ -0,0 +1,4 @@
+# AnalyticsRuby = Segment::Analytics.new(
+#   write_key: Settings.segmentio_write_key,
+#   on_error: Proc.new { |_status, msg| print msg }
+# )
---
+++ config/initializers/twilio_setup.rb
@@ -0,0 +1,2 @@
+TWILIO_ACCOUNT_SID = Settings.twilio_account_sid
+TWILIO_AUTH_TOKEN = Settings.twilio_auth_token
