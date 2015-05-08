10d6efe01c57ccb049efa6bb10555bcf69ba1838
Patrick O'Grady <ogradypatrickj@gmail.com>
2015-05-08 13:48:40 -0400

    Add models and services

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -9,4 +9,24 @@ class SubscriptionMailer < BaseMailer
     )
   end
 
+  def subscription_receipt(email, amount, stripe_invoice_id)
+    @amount = amount
+    @stripe_invoice_id = stripe_invoice_id
+
+    mail(
+      to: email,
+      subject: t("mailers.subscription.subscription_receipt.subject"),
+    )
+  end
+
+  def upcoming_payment_notification(subscription)
+    @subscription = subscription
+    @user = subscription.user
+
+    mail(
+      to: @user.email,
+      subject:
+        t("mailers.subscription.upcoming_payment_notification.subject")
+    )
+
 end
---
+++ app/models/subscription_payment_coming_up_notifier.rb
@@ -0,0 +1,13 @@
+class SubscriptionPaymentComingUpNotifier
+
+  def initialize(subscriptions)
+    @subscriptions = subscriptions
+  end
+
+  def process
+    @subscriptions.each do |subscription|
+      SubscriptionMailer.upcoming_payment_notification(subscription).deliver_now
+    end
+  end
+
+end
