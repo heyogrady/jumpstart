d92c44527499fe9c41313abfd01452bca1e5a119
Patrick O'Grady <ogradypatrickj@gmail.com>
2015-05-11 13:10:01 -0400

    Remove duplicate mailer

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -6,15 +6,4 @@ class BaseMailer < ActionMailer::Base
 
   default_url_options[:host] = Settings.host
 
-  def contact_us_notification(contact)
-    @email = contact.email
-    @title = contact.title
-    @body  = contact.body
-    subject = "Contact us message from #{@email}"
-
-    mail(to: Settings.support_email, from: @email,  subject: subject) do |format|
-      format.html
-    end
-  end
-
 end
